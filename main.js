fc2428835cec19b81f9d5dd3dfb2c899b07079e2
Terexit <teresamarbancastro@gmail.com>
2018-04-06 14:28:19 +0200

    first attempt on petitions

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict'
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json');
+
+request.addEventListener('load', getRecipe);
+
+function getRecipe() {
+  var response = request.responseText;
+  var responseJSON = JSON.parse(response);
+
+  var testing = document.querySelector('.testing');
+  img.src = responseJSON.message;
+}
+
+request.send();
